473758318caa60c5ebaead23ee1fef1168a322dd
Koppito <koppmatias97@gmail.com>
2018-02-25 12:46:25 -0300

    Agregado modelo y movimiento básico de jugador

---
+++ Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour {
+
+	public Transform target;
+
+	private Vector3 offset;
+
+	void Start() {
+		offset = transform.position;
+	}
+
+	void Update() {
+		transform.position = target.position + offset;
+	}
+
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float rotationSpeed = 0.5f;
+	public float anguloRotacion = 10.0f;
+	public float giroSpeed = 1.0f;
+	public Transform model;
+
+	void Start() {
+		speed /= 10.0f;
+	}
+
+	void FixedUpdate() {
+		transform.Translate (model.forward * speed);
+		if (Input.GetKey(KeyCode.A)) {
+			//Movimiento Jugador
+			transform.Translate (-model.right * speed);
+
+			//Rotacion player
+			Quaternion rotPlayer = Quaternion.Euler (transform.rotation.x, transform.rotation.y - giroSpeed, transform.rotation.z);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, rotPlayer, rotationSpeed);
+
+			//Rotacion modelo
+			Quaternion rotModelo = Quaternion.Euler (0.0f, 0.0f, anguloRotacion);
+			model.localRotation = Quaternion.RotateTowards (model.rotation, rotModelo, rotationSpeed);
+		}
+		else if (Input.GetKey(KeyCode.D)) {
+			//Movimiento Jugador
+			transform.Translate (model.right * speed);
+
+			//Rotacion player
+			Quaternion rotPlayer = Quaternion.Euler (transform.rotation.x, transform.rotation.y + giroSpeed, transform.rotation.z);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, rotPlayer, rotationSpeed);
+
+			//Rotacion modelo
+			Quaternion rotModelo = Quaternion.Euler (0.0f, 0.0f, -anguloRotacion);
+			model.localRotation = Quaternion.RotateTowards (model.rotation, rotModelo, rotationSpeed);
+		}
+		else {
+			//Resetear rotacion modelo
+			model.localRotation = Quaternion.RotateTowards (model.rotation, Quaternion.identity, rotationSpeed);
+		}
+
+	}
+
+}
