b93344dd40e775997d81f0e2cc1df05d8b36f336
Tino <cagustinclemente@gmail.com>
2018-02-26 13:43:26 -0300

    Reemplazado el script viejo de movimiento

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -3,48 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour {
+	public Joystick joystick;
+	private GameObject target;
+	private Rigidbody rb;
+	private float maxSpeed = 5;
+	private float speed = 4;
+	private Vector3 dir2;
 
-	public float speed = 1.0f;
-	public float rotationSpeed = 0.5f;
-	public float anguloRotacion = 10.0f;
-	public float giroSpeed = 1.0f;
-	public Transform model;
-
-	void Start() {
-		speed /= 10.0f;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		target = GameObject.Find ("Target");
 	}
+	
 
-	void FixedUpdate() {
-		transform.Translate (model.forward * speed);
-		if (Input.GetKey(KeyCode.A)) {
-			//Movimiento Jugador
-			transform.Translate (-model.right * speed);
+	void FixedUpdate () {
+		Vector3 dir = Vector3.zero;
+		dir.x = joystick.Horizontal();
+		dir.z = joystick.Vertical();
 
-			//Rotacion player
-			Quaternion rotPlayer = Quaternion.Euler (transform.rotation.x, transform.rotation.y - giroSpeed, transform.rotation.z);
-			transform.rotation = Quaternion.RotateTowards (transform.rotation, rotPlayer, rotationSpeed);
-
-			//Rotacion modelo
-			Quaternion rotModelo = Quaternion.Euler (0.0f, 0.0f, anguloRotacion);
-			model.localRotation = Quaternion.RotateTowards (model.rotation, rotModelo, rotationSpeed);
+		if (rb.velocity.magnitude > maxSpeed) {
+			rb.velocity = rb.velocity.normalized * maxSpeed;
 		}
-		else if (Input.GetKey(KeyCode.D)) {
-			//Movimiento Jugador
-			transform.Translate (model.right * speed);
+		if (dir.x != 0) {
+			/* dir.x es negativo si esta el joystick a la dereha, y positivo si esta a la izquierda
+			 * el problema radica cuando esta al centro, pero tiene movimiento en dir.z (hacia arriba o hacia abajo)
+			 * hay que buscar la forma de fusionar dir.x y dir.z
+			 */
 
-			//Rotacion player
-			Quaternion rotPlayer = Quaternion.Euler (transform.rotation.x, transform.rotation.y + giroSpeed, transform.rotation.z);
-			transform.rotation = Quaternion.RotateTowards (transform.rotation, rotPlayer, rotationSpeed);
-
-			//Rotacion modelo
-			Quaternion rotModelo = Quaternion.Euler (0.0f, 0.0f, -anguloRotacion);
-			model.localRotation = Quaternion.RotateTowards (model.rotation, rotModelo, rotationSpeed);
+			//Roto la referencia
+			target.transform.rotation = Quaternion.LookRotation (dir);
+			//Roto el barco, el valor al ultimo es la velocidad del giro
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, target.transform.rotation, Time.deltaTime * 20);
+			//Rotacion lateral
+			//BUG: Cuando el barco mira hacia nosotros hay qeu invertir la rotacion en z porque se inclina HACIA la curva
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, dir.x * 5);
+			//Aplico la fuerza hacia donde mira
+			rb.AddForce (transform.forward * speed);
+		} else {
+			//Reinicia la rotacion lateral
+			target.transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, 0);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, target.transform.rotation, Time.deltaTime * 15);
 		}
-		else {
-			//Resetear rotacion modelo
-			model.localRotation = Quaternion.RotateTowards (model.rotation, Quaternion.identity, rotationSpeed);
-		}
-
 	}
-
 }
